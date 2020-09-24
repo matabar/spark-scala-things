323582e21ae04448dc556d3493f6c5c1dac84bee
Matan-Philip-Baruch <matan.baruch.h8@gmail.com>
2020-09-25 00:43:59 +0300

    part 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,27 @@
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main {
+  System.setProperty("hadoop.home.dir", "C:/hadoop")
+  val spark: SparkSession = SparkSession.builder()
+    .appName("test")
+    .master("local[*]")
+    .getOrCreate()
+
+  implicit class implicitClass(df: DataFrame) {
+
+    def toJsonArray(length: Int, cols: String*): DataFrame = df
+      .withColumn("new_index", monotonically_increasing_id divide length cast "int")
+      .groupBy("new_index")
+      .agg(to_json(collect_list(struct(cols map col: _*))) as "json")
+      .drop("new_index")
+  }
+
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "C:/hadoop")
-    val spark = SparkSession.builder()
-      .appName("test")
-      .master("local[2]")
-      .getOrCreate()
-    spark.sparkContext.setLogLevel("WARN")
+    spark.read.format("csv")
+      .option("header", "true")
+      .load("src/main/resources/data.csv")
+      .toJsonArray(2, "col1", "col2", "col3")
+      .show()
   }
 }
